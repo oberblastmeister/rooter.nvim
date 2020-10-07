b3786b67ebedd2ae9c69406355bb06a65b35055c
Brian Shu <littlebubu.shu@gmail.com>
2020-10-07 15:00:58 -0400

    acutally fixed rooter

--- lua/rooter/init.lua
+++ lua/rooter/init.lua
@@ -1,27 +1,56 @@
-local rooter = {}
-
 local nvim_lsp = require('nvim_lsp')
-local configs = require('nvim_lsp/configs')
+local api = vim.api
 
--- local clients = vim.tbl_values(vim.lsp.buf_get_clients())
--- print(vim.inspect(clients))
+local rooter = {
+  cmd = [[autocmd VimEnter,BufReadPost,BufEnter * ++nested lua require'rooter'.root()]],
+  config = {
+    manual = false,
+    echo = true,
+    patterns = nvim_lsp.util.root_pattern('.git', 'Cargo.toml', 'go.mod'),
+    cd_command = 'lcd',
+    non_project_files == "current",
+    start_path = function()
+      return vim.fn.expand [[%:p:h]]
+    end,
+  }
+}
 
--- put this in on attach for each lsp client
-rooter.on_attach = function(client, options)
-  -- print(vim.inspect(client))
-  local root_dir = client.config.root_dir
-  vim.api.nvim_set_current_dir(root_dir)
-
-  if options ~= nil then
-    if options.rooter_echo == true then
-      print('[rooter] changing to directory', root_dir)
+function rooter.root()
+  local config = rooter.config
+  if vim.bo.filetype == config.filetypes_exclude then
+    return
+  end
+  local res = config.patterns(config.start_path())
+  if res then
+    if config.echo then
+      print("[rooter] changing directory to" .. " " .. res)
     end
+    vim.cmd(config.cd_command .. " " .. res)
+  elseif config.non_project_files == "current" then
+    if config.echo then
+      print("[rooter] changing directory to current file")
+    end
+    local current = vim.fn.expand [[%:p:h]]
+    vim.cmd(config.cd_command .. " " .. current)
+  elseif config.non_project_files == "home" then
+    if config.echo then
+      print("[rooter] changing directory to home")
+    end
+    vim.cmd(config.cd_command .. " " .. "~")
   end
 end
 
-rooter.root = function()
-  local configs = require('nvim_lsp/configs')
-  print(vim.inspect(configs))
+function rooter.set_config(cfg)
+  rooter.config = vim.tbl("keep", cfg, rooter.config)
+end
+
+function rooter.setup()
+  if not rooter.config.manual then
+    vim.cmd [[augroup rooter]]
+    vim.cmd [[autocmd!]]
+    vim.cmd(rooter.cmd)
+    vim.cmd [[augroup END]]
+  end
 end
 
 return rooter
